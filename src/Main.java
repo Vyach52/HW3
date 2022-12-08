939177b105700868dc52224cad7762f363fc8ffe
Vyach52 <Sslavih81@gmail.com>
2022-12-08 20:59:46 +0300

    Вторая часть переменных

---
+++ src/Main.java
@@ -0,0 +1,90 @@
+import javax.sound.sampled.SourceDataLine;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static  void  main(String[] args)
+    {
+    int a = 10 ;
+    byte b =  11;
+    short c = 12;
+    long d = 1000;
+    float e = 3.88f;
+    double h = 14.88;
+System.out.println("Значение переменной с типом int "+" равно "+a);
+System.out.println("Значение переменной с типом byte "+" равно "+b);
+System.out.println("Значение переменной с типом short "+" равно "+c);
+System.out.println("Значение переменной с типом  long "+" равно "+d);
+System.out.println("Значение переменной с типом float "+" равно "+e);
+System.out.println("Значение переменной с типом double"+" равно "+h);
+System.out.println(
+
+);
+byte a1 =67;
+float b1 = 987678965549f;
+float c1 = 2.786f;
+short d1 = 569;
+short e1 = -159;
+int h1 = 27897;
+        System.out.println(
+
+        );
+        System.out.println("Задача про распределение бумаги между учениками");
+        byte student = 23+27+30;
+        int bum = 480;
+        long l = bum/student;
+        System.out.println("На каждого ученика рассчитано " +l+" листов бумаги");
+        System.out.println(
+
+        );
+        System.out.println("Задача про краску");
+        byte ban1cl = 2 + 4;
+        int cl = 120 / ban1cl;
+        int belKr = 2 * cl;
+        int korKr = 4 * cl;
+        System.out.println("В школе, где "+cl+" классов, нужно "+belKr+" банок белойй краски и "+korKr+" банок коричневой краски");
+        System.out.println(
+
+        );
+        System.out.println("Про завтрак атлета");
+        int massBananas = 80 * 5;
+        int massMilk = 2 * 105;
+        int massCream = 2 * 100;
+        int massEggs = 4 * 75;
+        int totalMass = massCream + massBananas + massEggs + massMilk;
+        double tm = totalMass * 0.001;
+        System.out.println(totalMass+" грамм или "+tm+" килограмм" );
+        System.out.println(
+
+        );
+        System.out.println("Про сгонку веса");
+        int x1 = (7*1000)/250;
+        int x2 = (7*1000)/500;
+        int x3 = (x1+x2)/2;
+        System.out.println("При потере в весе по 250 грамм в день, уйдет "+x1+" дней. При потере в весе в 500 гамм в день , потребуется "+x2+" дней." +
+                "Так потеря 7 кг собственного веса занимает в среднем "+x3+" дней");
+        System.out.println(
+
+        );
+        System.out.println("Про доход");
+        double m1mes = 67760+(67760*0.1);
+        int m12mes1 = 67760*12;
+        double m12mes2 = m1mes *12;
+        double deltaM = m12mes2 - m12mes1;
+        System.out.println("Мария теперь получает "+m1mes+" рублей. Годовой доход вырос на "+deltaM);
+        double d1mess = 83690+(83690*0.1);
+        int d12mes1 = 83690*12;
+        double d12mes2 = d1mess*12;
+        double deltaD = d12mes2-d12mes1;
+        System.out.println("Денис теперь получает "+d1mess+" рублей. Готовой доход вырос на "+deltaD);
+        double k1mes = 76230+(76230*0.1);
+        int k12mes1 = 76230*12;
+        double k12mes2 = k1mes * 12;
+        double deltaK = k12mes2 - k12mes1;
+        System.out.println("Кристина теперь получает "+k1mes+" рублей. Годовой доход вырос на "+deltaK);
+
+
+
+
+
+
+}}
